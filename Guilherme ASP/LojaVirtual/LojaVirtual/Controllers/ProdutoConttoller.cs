d3e76684ffdff2d6f7f80999619ee562720c50cc
guilhermedp <55446382+guilhermedp@users.noreply.github.com>
2019-10-03 15:09:34 -0300

    Alteração para Aspnet 2.2

---
+++ Guilherme ASP/LojaVirtual/LojaVirtual/Controllers/ProdutoConttoller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoConttoller : Controller
+    {
+        /*
+         * ActionResult
+         * IActionResul.
+         * 
+         */
+        public ActionResult Visualizar()
+        {
+            return  new ContentResult() { Content =  "<h3> Produto - > Visualiza</h3>",ContentType = "text/html" };
+        }
+    }
+
+}
